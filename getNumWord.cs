28a9ed5e8aafcbe971f7aa6f2db714b0670883f1
siyabonga konile <siyabongasiya306@gmail.com>
2019-05-09 15:50:32 +0200

    Update getNumWord.cs

--- getNumWord.cs
+++ getNumWord.cs
@@ -1,1 +1,27 @@
+using System;
 
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine(GetNumWords("Hello, World!"));
+  }
+  
+  static int GetNumWords(string sStr)
+  {
+    int iNumWords = 0;
+    bool isSpaceAvailable = true;
+    int iIndex;
+    while(isSpaceAvailable)
+    {
+      iIndex = sStr.IndexOf(" ");
+      isSpaceAvailable = (iIndex == -1)? false: true;
+      if(isSpaceAvailable)
+      {
+        iNumWords++;
+        sStr = sStr.Substring(iIndex + 2);
+      }
+    }
+    return iNumWords;
+  }
+}
